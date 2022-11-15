301e0aef4fae4013aa24f36a6fd2e5dae7e56a77
AnnDutova <ann.dutova@list.ru>
2022-11-16 00:18:29 +0300

    Solve empty string problem add insertOrUpdate function

--- connectorJIRA/pkg/datapusher/data_pusher.go
+++ connectorJIRA/pkg/datapusher/data_pusher.go
@@ -19,7 +19,7 @@ func New(db_ *sql.DB) (*PSQLConnector, error) {
 	}, nil
 }
 
-func (c *PSQLConnector) PushFirstData(issues []datatransformer.Issue, statuses []datatransformer.IssueStatusChanges) error {
+func (c *PSQLConnector) UpdateData(issues []datatransformer.Issue, statuses []datatransformer.IssueStatusChanges) error {
 	if err := c.pushIssues(issues); err != nil {
 		return err
 	}
@@ -32,7 +32,7 @@ func (c *PSQLConnector) PushFirstData(issues []datatransformer.Issue, statuses [
 func (c *PSQLConnector) UpdateIssue(issue datatransformer.Issue, statuses datatransformer.IssueStatusChanges) error {
 	command := fmt.Sprintf("select updateIssue('%d','%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
 		issue.Id, issue.Assignee, issue.UpdatedTime, issue.ClosedTime,
-		issue.Summary, issue.Description, issue.Type, issue.Priority, issue.Status)
+		strings.ReplaceAll(issue.Summary, "'", ""), strings.ReplaceAll(issue.Description, "'", ""), issue.Type, issue.Priority, issue.Status)
 	var oldDate time.Time
 	if err := c.db.QueryRow(command).Scan(&oldDate); err != nil {
 		return err
@@ -52,10 +52,10 @@ func (c *PSQLConnector) UpdateIssue(issue datatransformer.Issue, statuses datatr
 
 func (c *PSQLConnector) pushIssues(issues []datatransformer.Issue) error {
 	for _, val := range issues {
-		command := fmt.Sprintf("call insertIssue('%d','%s','%d','%s','%s','%s','%s','%s','%s', '%s', '%s', '%s', '%s', '%s', '%d')",
+		command := fmt.Sprintf("call insertOrUpdateIssue('%d','%s','%d','%s','%s','%s','%s','%s','%s', '%s', '%s', '%s', '%s', '%s', '%d')",
 			val.Id, val.Project.Name, val.Project.Id, val.Creator, val.Assignee, val.Key,
 			val.CreatedTime.Format(time.RFC3339), val.ClosedTime.Format(time.RFC3339), val.UpdatedTime.Format(time.RFC3339),
-			val.Summary, strings.ReplaceAll(val.Description, "'", ""), val.Type, val.Priority, val.Status,
+			strings.ReplaceAll(val.Summary, "'", ""), strings.ReplaceAll(val.Description, "'", ""), val.Type, val.Priority, val.Status,
 			val.TimeSpent)
 		_, err := c.db.Exec(command)
 		if err != nil {
@@ -67,12 +67,19 @@ func (c *PSQLConnector) pushIssues(issues []datatransformer.Issue) error {
 
 func (c *PSQLConnector) pushStatusChanges(statuses []datatransformer.IssueStatusChanges) error {
 	for _, val := range statuses {
+		command := fmt.Sprintf("select getLastChangeTime('%d')", val.Id)
+		var oldDate time.Time
+		if err := c.db.QueryRow(command).Scan(&oldDate); err != nil {
+			return err
+		}
 		for _, history := range val.Histories {
-			command := fmt.Sprintf("call insertStatusChange('%s','%d','%s','%s','%s')", history.Author, val.Id,
-				history.ChangeTime.Format(time.RFC3339), history.FromStatus, history.ToStatus)
-			_, err := c.db.Exec(command)
-			if err != nil {
-				return err
+			if history.ChangeTime.After(oldDate) {
+				command := fmt.Sprintf("call insertStatusChange('%s','%d','%s','%s','%s')", history.Author, val.Id,
+					history.ChangeTime.Format(time.RFC3339), history.FromStatus, history.ToStatus)
+				_, err := c.db.Exec(command)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
--- db/db-init-scripts/2-db-functions.sql
+++ db/db-init-scripts/2-db-functions.sql
@@ -1,69 +1,70 @@
 create or replace procedure insertIssue(issue_id int, project_title text, project_id int, author_name text, assigned_name text,
-key_ text, createTime_ timestamp, closedTime_ timestamp, updatedTime_ timestamp, summary_ text, description_ text, type_ text,
-priority_ text, status_  text, timeSpent_ bigint)
+                                        key_ text, createTime_ timestamp, closedTime_ timestamp, updatedTime_ timestamp, summary_ text, description_ text, type_ text,
+                                        priority_ text, status_  text, timeSpent_ bigint)
     LANGUAGE plpgsql
 as $$
-    Declare author_id int;
+Declare author_id int;
     Declare assignee_id int;
 begin
     if exists (select id from author where author.name=author_name) then
-Select id into author_id from author where author.name=author_name;
-else
-select insertAuthor(author_name) into author_id;
-end if;
+        Select id into author_id from author where author.name=author_name;
+    else
+        select insertAuthor(author_name) into author_id;
+    end if;
 
     if exists (select id from author where author.name=assigned_name) then
-Select id into assignee_id from author where author.name=assigned_name;
-else
-select insertAuthor(assigned_name) into assignee_id;
-end if;
+        Select id into assignee_id from author where author.name=assigned_name;
+    else
+        if length(assigned_name) > 0 then
+            select insertAuthor(assigned_name) into assignee_id;
+        end if;
+    end if;
 
     if not exists (select id from project where project.id=project_id) then
         perform insertProject(project_title, project_id);
-end if;
+    end if;
 
-Insert Into issues(id, projectId, authorId, assigneeId, key, createdTime,closedTime, updatedTime, summary,
-                   description, type, priority, status,timeSpent) values
-    (issue_id, project_id, author_id,assignee_id, key_,createTime_,closedTime_,updatedTime_, summary_, description_,
-     type_,priority_,status_, timeSpent_);
-commit;
+    Insert Into issues(id, projectId, authorId, assigneeId, key, createdTime,closedTime, updatedTime, summary,
+                       description, type, priority, status,timeSpent) values
+        (issue_id, project_id, author_id,assignee_id, key_,createTime_,closedTime_,updatedTime_, summary_, description_,
+         type_,priority_,status_, timeSpent_);
+    commit;
 end;
 $$;
 
 create or replace procedure insertStatusChange(author_name text, issue_id int,
-                                              changeTime_ timestamp, fromStatus_ text, toStatus_ text)
+                                               changeTime_ timestamp, fromStatus_ text, toStatus_ text)
     LANGUAGE plpgsql
 as $$
 Declare author_id int;
 begin
-Select id into author_id from author where author.name=author_name;
-Insert Into "statusChange"(issueId, authorId, changeTime, fromStatus, toStatus)
-Values(issue_id, author_id, changeTime_,fromStatus_, toStatus_);
+    Select id into author_id from author where author.name=author_name;
+    Insert Into "statusChange"(issueId, authorId, changeTime, fromStatus, toStatus)
+    Values(issue_id, author_id, changeTime_,fromStatus_, toStatus_);
 end;
 $$;
 
-create or replace function updateIssue(issue_id int, assignee_name text, new_updatedTime_ timestamp, closedTime_ timestamp,  summary_ text,
-description_ text, type_ text, priority_ text, status_  text) returns timestamp
+create or replace procedure updateIssue(issue_id int, assignee_name text, new_updatedTime_ timestamp, closedTime_ timestamp,  summary_ text,
+                                        description_ text, type_ text, priority_ text, status_  text, timeSpent_ bigint)
     LANGUAGE plpgsql
 as $$
-    Declare assignee_id int;
+Declare assignee_id int;
     Declare old_date timestamp;
 begin
     if exists (select id from author where author.name=assignee_name) then
-Select id into assignee_id from author where author.name=assignee_name;
-else
-select insertAuthor(assignee_name) into assignee_id;
-end if;
+        Select id into assignee_id from author where author.name=assignee_name;
+    else
+        select insertAuthor(assignee_name) into assignee_id;
+    end if;
 
-select updatedTime into old_date from issues where id = issue_id;
+    select updatedTime into old_date from issues where id = issue_id;
 
-if (select updatedTime from issues where id = issue_id) < new_updatedTime_ then
-Update issues set (authorId, updatedTime, closedTime, summary, description, type, priority, status) =
-                      (assignee_id, new_updatedTime_, closedTime_, summary_, description_, type_, priority_, status_)
-where  id = issue_id;
-end if;
-commit;
-return old_date;
+    if (select updatedTime from issues where id = issue_id) < new_updatedTime_ then
+        Update issues set (authorId, updatedTime, closedTime, summary, description, type, priority, status, timeSpent) =
+                              (assignee_id, new_updatedTime_, closedTime_, summary_, description_, type_, priority_, status_, timeSpent_)
+        where  id = issue_id;
+    end if;
+    commit;
 end;
 $$;
 
@@ -77,8 +78,8 @@ create or replace function returnProjectTitle(id_ int) returns text
 as $$
 Declare project_title text;
 BEGIN
-Select title into project_title from project where project.id=id_;
-return project_title;
+    Select title into project_title from project where project.id=id_;
+    return project_title;
 END;
 $$;
 
@@ -93,8 +94,41 @@ create or replace function returnAuthorName(id_ int) returns text
 as $$
 Declare author_name text;
 BEGIN
-Select name into author_name from author where author.id=id_;
-return author_name;
+    Select name into author_name from author where author.id=id_;
+    return author_name;
 END;
 $$;
 
+create or replace procedure insertOrUpdateIssue(issue_id int, project_title text, project_id int, author_name text, assigned_name text,
+                                                key_ text, createTime_ timestamp, closedTime_ timestamp, updatedTime_ timestamp, summary_ text, description_ text, type_ text,
+                                                priority_ text, status_  text, timeSpent_ bigint)
+    LANGUAGE plpgsql
+as $$
+BEGIN
+    if exists (select id from issues where issue_id=issues.id) then
+        Call updateIssue(issue_id, assigned_name ,
+                         updatedTime_, closedTime_,summary_, description_, type_,
+                         priority_, status_, timeSpent_);
+    else
+        call insertIssue(issue_id, project_title, project_id, author_name, assigned_name ,
+                         key_, createTime_, closedTime_, updatedTime_, summary_, description_, type_,
+                         priority_, status_ , timeSpent_);
+    end if;
+    commit;
+END;
+$$;
+
+create or replace function getLastChangeTime(id_ int) returns timestamp
+    LANGUAGE plpgsql
+as $$
+Declare lastTime timestamp;
+BEGIN
+    if exists(select * from "statusChange" where "statusChange".issueid=id_) then
+        select "statusChange".changetime into lastTime from "statusChange" where "statusChange".issueid=id_
+        order by "statusChange".changetime desc;
+    else
+        select to_timestamp(0) into lastTime;
+    end if;
+    return lastTime;
+END;
+$$;
